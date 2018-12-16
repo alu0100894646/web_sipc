72e4e94df214fc90c0edfd6ae907eaed9104fc40
José Fco Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-12-16 20:40:10 +0000

    Añadida la barra pero no se queda sticky al hacer scroll

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 // JavaScript source code
 //codigo js para el index
+
+window.onscroll = function () { scrollFunction() };
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function scrollFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    }else {
+        navbar.classList.remove("sticky");
+    }
+}
